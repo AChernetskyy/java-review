68d0e55fef4f5d556c1a4cab7dafbc13a1a5ca90
AChernatskyy <directvusa2010@gmail.com>
2023-02-23 21:16:30 -0500

    commit

---
+++ src/test/java/com/review/javareview/JavaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.javareview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
