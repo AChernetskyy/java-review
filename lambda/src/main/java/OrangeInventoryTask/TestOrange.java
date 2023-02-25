105ddfa3cb73aca7a9533bb4e1720e5d54d5e2ae
AChernatskyy <directvusa2010@gmail.com>
2023-02-25 16:56:19 -0500

    Funcional Interface

--- lambda/src/main/java/OrangeInventoryTask/TestOrange.java
+++ lambda/src/main/java/OrangeInventoryTask/TestOrange.java
@@ -2,6 +2,8 @@ package OrangeInventoryTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class TestOrange {
     public static void main(String[] args) {
@@ -12,17 +14,21 @@ public class TestOrange {
         inventory.add(Orange.builder().weight(100).color(Color.RED).build());
         inventory.add(Orange.builder().weight(400).color(Color.YELLOW).build());
 
-        OrangeFilter<Orange> getColor = orange -> "A " + (orange.getWeight() < 150 ? "Light" : "Heavy") + " " + orange.getColor() + " Orange";
+       // OrangeFilter<Orange> getColor = orange -> "A " + (orange.getWeight() < 150 ? "Light" : "Heavy") + " " + orange.getColor() + " Orange";
+        //Implementation by using Java ready interfaces -->Function
+        Function<Orange, String> getColor = orange -> "A " + (orange.getWeight() < 150 ?"Light" : "Heavy") + " " + orange.getColor() + " Orange";
         formatedInventory(inventory, getColor);
 
-        OrangeFilter<Orange> getWeight = orange -> "An orange of "+orange.getWeight()+" g";
+        //OrangeFilter<Orange> getWeight = orange -> "An orange of "+orange.getWeight()+" g";
+        //Implementation by using Java ready interfaces-->Function
+        Function<Orange, String> getWeight = orange -> "An orange of "+orange.getWeight()+" g";
         formatedInventory(inventory, getWeight);
 
     }
 
-    public static void formatedInventory(List<Orange>list, OrangeFilter filter){
+    public static void formatedInventory(List<Orange>list, Function filter){
         for (Orange orange: list) {
-            System.out.println(filter.input(orange));
+            System.out.println(filter.apply(orange));
         }
     }
 }
