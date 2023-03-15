4b5041caeacae67b70963bd222d8fe0084a2c52c
AChernatskyy <directvusa2010@gmail.com>
2023-03-14 21:18:26 -0400

    collectors practice

--- streams/src/main/java/JavaCollectors.java
+++ streams/src/main/java/JavaCollectors.java
@@ -8,26 +8,26 @@ import java.util.stream.Collectors;
 public class JavaCollectors {
     public static void main(String[] args) {
 
-        List<Integer>numbers = Arrays.asList(3,4,5,6,6,7);
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 6, 6, 7);
 
         //toCollection(Supplier): is used to create a collection using collector and save it to new ArrayList
-        List<Integer>newList = numbers.stream().filter(x->x%2==0).collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> newList = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toCollection(ArrayList::new));
 
         //.toMap(Function, Function) : returns a Collector interface that gathers the input data into the Map
         Map<String, Integer> dish = DishData.dishList().stream().collect(Collectors.toMap(Dish::getName, Dish::getCalories));
         System.out.println(dish);
 
         //.counting() : returns a Collector that counts the number of the elements
-        Long num =   numbers.stream().filter(p->p%2==0).collect(Collectors.counting());
+        Long num = numbers.stream().filter(p -> p % 2 == 0).collect(Collectors.counting());
 
         //.summingInt(ToIntFunction) : returns a Collector with sum of integers value
-       Long num1 =  DishData.dishList().stream().map(Dish::getCalories).collect(Collectors.counting());
+        Long num1 = DishData.dishList().stream().map(Dish::getCalories).collect(Collectors.counting());
 
-       //.averagingInt(ToIntFunction) : returns average of the integers
+        //.averagingInt(ToIntFunction) : returns average of the integers
 
-       DishData.dishList().stream().collect(Collectors.averagingInt(Dish::getCalories));
+        DishData.dishList().stream().collect(Collectors.averagingInt(Dish::getCalories));
 
-       //.joining() : is used to join various elements of a character or string array into single object
+        //.joining() : is used to join various elements of a character or string array into single object
         List<String> strList = Arrays.asList("John", "Gary", "Gill");
         strList.stream().collect(Collectors.joining());
 
@@ -35,7 +35,13 @@ public class JavaCollectors {
         DishData.dishList().stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
 
         //.groupingBy() : is used for grouping
-        Map<Type, List<Dish>> dishType =   DishData.dishList().stream().collect(Collectors.groupingBy(Dish::getType));
+        Map<Type, List<Dish>> dishType = DishData.dishList().stream().collect(Collectors.groupingBy(Dish::getType));
         System.out.println(dishType);
+
+        Map<Integer, Long> nameCount = DishData.dishList().stream().filter(each -> each.getCalories() > 500).collect(Collectors.groupingBy(Dish::getCalories, Collectors.counting()));
+        System.out.println(nameCount);
+        System.out.println("*******************");
+        Map<String, Double> avgCal = DishData.dishList().stream().collect(Collectors.groupingBy(Dish::getName, Collectors.averagingInt(Dish::getCalories)));
+        System.out.println(avgCal);
     }
 }
